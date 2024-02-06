3fae578ca7bb42c53a3cd29e702f08e1eacbe217
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-02-06 15:37:39 +0530

    added email validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,97 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/rendering.dart';
-import 'package:twitter_clone/pages/first.dart';
-import 'package:twitter_clone/pages/fourth.dart';
-import 'package:twitter_clone/pages/second.dart';
-import 'package:twitter_clone/pages/third.dart';
-import 'package:twitter_clone/pages/fifth.dart';
 
-void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();S
-  // await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Twitter Clone',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      routes: {
-        '/': (context) => const MyHomePage(),
-        '/first': (context) => const FirstPage(),
-        '/fourth': (context) => const FourthPage(),
-        '/fifth': (context) => const FifthPage(),
-      },
+      home: const MyHomePage(title: 'Twitter Clone'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _signInKey = GlobalKey();
+  // Defining controller for email and password
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final RegExp emailValid = RegExp(
+      r"^ [a-zA-Z0-9.a-zA-Z0-9. 1#$%&'*+-/=?^_ {|]~]+@ [a-zA-Z0-9] +\. [a-zA-Z]+");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Navigation'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
       ),
-      body: Center(
+      body: Form(
+        key: _signInKey,
         child: Column(
           children: <Widget>[
-            // Named Navigation
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed("/first");
+            // Reason for using TextFormField is, it has special property called validator
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: 'Enter an email',
+                labelText: 'email',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter an email';
+                } else if (!emailValid.hasMatch(value)) {
+                  return 'Please enter a valid email';
+                }
+                return null;
               },
-              child: const Text("First Page"),
             ),
-
-            // Simple Navigation
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => const SecondPage()),
-                );
+            TextFormField(
+              controller: _passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'password',
+                hintText: 'Enter a password',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'please enter a valid password';
+                } else if (value.length < 6) {
+                  return 'Minumum Characters: 6';
+                }
+                return null;
               },
-              child: const Text("Second Page"),
             ),
-
-            // Navigation With data using Basic Navigation
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const ThirdPage(name: 'Sai Teja'),
-                  ),
-                );
-              },
-              child: const Text("Third Page"),
-            ),
-
-            // Navigatin With data using named navigation
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/fourth", arguments: "Sai Teja");
-              },
-              child: const Text("Fourth Page"),
-            ),
-            TextButton(
-              onPressed: () async {
-                final response = await (Navigator.pushNamed(context, "/fifth",
-                    arguments: "Sai Teja"));
-                print(response);
-              },
-              child: const Text("Fifth Page"),
-            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_signInKey.currentState!.validate()) {
+                    print('Email: ${_emailController.text}'); // Retrieving data
+                    print('Password: ${_passwordController.text}');
+                  }
+                },
+                child: const Text('Submit'))
           ],
         ),
       ),

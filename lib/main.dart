1b5187a14d1fd09118f3b59fde11e0dfe3176073
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-02-07 05:06:51 +0530

    added singup page and branding

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,63 +36,116 @@ class _MyHomePageState extends State<MyHomePage> {
   // Defining controller for email and password
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final RegExp emailValid = RegExp(
-      r"^ [a-zA-Z0-9.a-zA-Z0-9. 1#$%&'*+-/=?^_ {|]~]+@ [a-zA-Z0-9] +\. [a-zA-Z]+");
+  final RegExp emailValid = RegExp(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Form(
         key: _signInKey,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Image(
+              image: AssetImage('assets/Logo_of_Twitter.svg.png'),
+              width: 90,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Login to twitter',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             // Reason for using TextFormField is, it has special property called validator
-            TextFormField(
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Enter an email',
-                labelText: 'email',
-                border: OutlineInputBorder(),
+            Container(
+              margin: const EdgeInsets.fromLTRB(15, 30, 15, 0),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(30),
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter an email';
-                } else if (!emailValid.hasMatch(value)) {
-                  return 'Please enter a valid email';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'password',
-                hintText: 'Enter a password',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'please enter a valid password';
-                } else if (value.length < 6) {
-                  return 'Minumum Characters: 6';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  if (_signInKey.currentState!.validate()) {
-                    print('Email: ${_emailController.text}'); // Retrieving data
-                    print('Password: ${_passwordController.text}');
+              child: TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: 'Enter an email',
+                  border: InputBorder.none,
+                  labelText: 'email',
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  } else if (!emailValid.hasMatch(value)) {
+                    return 'Please enter a valid email';
                   }
+                  return null;
                 },
-                child: const Text('Submit'))
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextFormField(
+                controller: _passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'password',
+                  hintText: 'Enter a password',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'please enter a valid password';
+                  } else if (value.length < 6) {
+                    return 'Minumum Characters: 6';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Container(
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextButton(
+                  onPressed: () {
+                    if (_signInKey.currentState!.validate()) {
+                      print(
+                          'Email: ${_emailController.text}'); // Retrieving data
+                      print('Password: ${_passwordController.text}');
+                    }
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  )),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const SignUp(
+                          title: 'SignUp',
+                        )));
+              },
+              child: const Text('dont have and account? Sign Up here'),
+            ),
           ],
         ),
       ),

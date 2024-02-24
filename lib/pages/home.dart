3f053f6dc38ef2b502f8e72bec1987971c61a7ac
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-02-24 10:56:28 +0530

    added drawer, setting page, username and proifle pic fields

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,32 +2,64 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/providers/user_provider.dart';
+import 'package:twitter_clone/pages/settings.dart';
 
 class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    LocalUser currentUser = ref.watch(userProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              ref.read(userProvider.notifier).logOut();
-            },
-            child: const Text('Sign Out'),
-          )
-        ],
+        leading: Builder(builder: (context) {
+          return GestureDetector(
+            onTap: () => Scaffold.of(context).openDrawer(),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(currentUser.user.profilePic),
+              ),
+            ),
+          );
+        }),
       ),
       body: Column(
         children: [
-          Text(ref.watch(userProvider).user.email),
-          Text(ref.watch(userProvider).user.name),
-          CircleAvatar(backgroundImage: NetworkImage(ref.watch(userProvider).user.profilePic)),
+          Text(currentUser.user.email),
+          Text(currentUser.user.name),
         ],
       ),
+      drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: Column(
+            children: [
+              Image.network(currentUser.user.profilePic),
+              ListTile(
+                title: Text(
+                  "Hello, ${currentUser.user.name}",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+              ListTile(
+                title: const Text("Settings"),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Settings()));
+                },
+              ),
+              ListTile(
+                title: const Text("Sign Out"),
+                onTap: () {
+                  FirebaseAuth.instance.signOut();
+                  ref.read(userProvider.notifier).logOut();
+                },
+              )
+            ],
+          )),
     );
   }
 }
